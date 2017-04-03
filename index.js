a69a59d7a8e87d3281d9799d2d0ddfdd148d4d48
Duc Do <dotrananhduc@gmail.com>
2017-04-03 14:57:05 +0000

    add check shortened url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express()
 var mongoose = require("mongoose");
-
+var Url = require("./model");
 var mongoURL = process.env.MONGOLAB_URI || "mongodb://localhost:27017/url-shorten";
 
 //connect to db
@@ -32,14 +32,31 @@ app.get('/', function(req, res) {
   });
 })
 
+//shortened URL
 app.get('/:query', function (req, res) {
   var url = req.params.query;
+  var id = parseInt(url, 10);
+  
+  if(Number.isNaN(id)){
+    res.status(404).send("Invalid short URL");
+  }
+  else {
+    Url.find({id: id}, function(err, docs){
+      if(err) console.log(err);
+      if(docs && docs.length){
+        res.redirect(docs[0].url);
+      }
+      else{
+        res.status(404).send("Invalid URL");
+      }
+    })
+  }
 })
 
 app.get('/new/:url*',function(req, res) {
     
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
